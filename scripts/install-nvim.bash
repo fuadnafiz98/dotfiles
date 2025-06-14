7837208d3a80f2140d9922cf682dde83e456981d
fuadnafiz98 <fuadnafiz98@gmail.com>
2025-06-14 21:13:34 +0600

    chore: install scripts

---
+++ scripts/install-nvim.bash
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
---
+++ scripts/install.bash
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+curl -fsSL https://bun.sh/install | bash
+
+curl -fsSL https://fnm.vercel.app/install | bash
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+
